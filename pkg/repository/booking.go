e2fb7acc6463513f6043bfc3f0032efde1978536
Danil Konkov <BuroRoll@gmail.com>
2022-03-21 18:04:51 +0500

    add booking status changer and bookings by menti

--- pkg/repository/booking.go
+++ pkg/repository/booking.go
@@ -64,28 +64,28 @@ type UserBooking struct {
 	Status    string
 	MentiId   uint
 
-	//Duration15   bool
-	//Price15      uint
-	//Duration30_1 bool
-	//Price30_1    uint
-	//Duration30_3 bool
-	//Price30_3    uint
-	//Duration30_5 bool
-	//Price30_5    uint
-	//
-	//Duration60_1 bool
-	//Price60_1    uint
-	//Duration60_3 bool
-	//Price60_3    uint
-	//Duration60_5 bool
-	//Price60_5    uint
-	//
-	//Duration90_1 bool
-	//Price90_1    uint
-	//Duration90_3 bool
-	//Price90_3    uint
-	//Duration90_5 bool
-	//Price90_5    uint
+	Duration15   bool
+	Price15      uint
+	Duration30_1 bool
+	Price30_1    uint
+	Duration30_3 bool
+	Price30_3    uint
+	Duration30_5 bool
+	Price30_5    uint
+
+	Duration60_1 bool
+	Price60_1    uint
+	Duration60_3 bool
+	Price60_3    uint
+	Duration60_5 bool
+	Price60_5    uint
+
+	Duration90_1 bool
+	Price90_1    uint
+	Duration90_3 bool
+	Price90_3    uint
+	Duration90_5 bool
+	Price90_5    uint
 
 	Time []models.BookingTime `gorm:"foreignKey:BookingClassID;references:ID"`
 
