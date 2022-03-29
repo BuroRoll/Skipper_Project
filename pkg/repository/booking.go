9cf9e8ccc7a323458543010f23d41ab7daa3dfee
Danil Konkov <BuroRoll@gmail.com>
2022-03-29 13:07:02 +0500

    n

--- pkg/repository/booking.go
+++ pkg/repository/booking.go
@@ -118,12 +118,13 @@ func (b BookingPostgres) GetBookingsToMe(mentorId uint, status string) ([]UserBo
 		Preload("Class.Tags").
 		Preload("Time").
 		Select("*").
-		Where("user_id=? AND status = ?", mentorId, status).
+		Where("user_id=? AND status = ? AND new_data.messenger_id = messenger_data.messenger_id", mentorId, status).
 		Joins("LEFT JOIN (select id as user_data_id, first_name, second_name, time as user_time from Users) AS menti_data ON user_classes.menti_id = menti_data.user_data_id").
-		Joins("LEFT JOIN (SELECT parent_id, login AS communication_login, id AS communication_id FROM communications) AS communications_data ON parent_id = menti_data.user_data_id").
-		//Joins("LEFT JOIN (SELECT id AS messenger_id, name AS messenger_name FROM messengers) AS messenger_data ON messenger_id = communication").
 		Joins("LEFT JOIN (SELECT messenger_id, communication_id as cmc_id FROM messenger_communication) AS messager_communications ON messager_communications.cmc_id = communication").
+		//Joins("LEFT JOIN (SELECT id AS messenger_id, name AS messenger_name FROM messengers) AS messenger_data ON messenger_id = communication").
 		Joins("LEFT JOIN (SELECT id AS messenger_id, name AS messenger_name FROM messengers) AS messenger_data ON messager_communications.messenger_id = messenger_data.messenger_id").
+		Joins("LEFT JOIN (SELECT parent_id, login AS communication_login, id AS communication_id FROM communications) AS communication_data ON parent_id = menti_data.user_data_id").
+		Joins("LEFT JOIN (SELECT communication_id AS n_id, messenger_id FROM messenger_communication) AS new_data ON new_data.n_id = communication_id").
 		Find(&bookings)
 	return bookings, nil
 }
