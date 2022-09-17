e6c0cb90fc7f96a4f2813df855520f5fbb3e00c1
Danil Konkov <BuroRoll@gmail.com>
2022-09-17 15:09:20 +0500

    add docker-compose.yaml

--- pkg/models/base.go
+++ pkg/models/base.go
@@ -11,7 +11,7 @@ import (
 var db *gorm.DB
 
 func init() {
-	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
+	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable port=%s password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_PASSWORD"))
 	conn, err := gorm.Open(postgres.Open(dbUri), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("error %s", err)
